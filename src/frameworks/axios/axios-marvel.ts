064cc02d6b8681d474ccced60279d8e1985e0acd
Gondolier22 <cristian.portillosanchez@gmail.com>
2025-02-27 17:10:05 +0100

    feat: creand instacia de axios

---
+++ src/frameworks/axios/axios-marvel.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const axiosMarvel = axios.create({
+  baseURL: "https://gateway.marvel.com",
+  params: {
+    ts: 1,
+    apikey: import.meta.env.VITE_MARVEL_API_KEY,
+    hash: import.meta.env.VITE_MARVEL_API_HASH,
+  },
+});
