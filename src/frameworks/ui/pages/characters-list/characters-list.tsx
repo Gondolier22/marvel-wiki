ed90376faed2a6211d7a0eccdd8cf08df0d297f4
Gondolier22 <cristian.portillosanchez@gmail.com>
2025-02-28 08:58:37 +0100

    feat: maquetacion de lista de personajes con buscador terminada

--- src/frameworks/ui/pages/characters-list/characters-list.tsx
+++ src/frameworks/ui/pages/characters-list/characters-list.tsx
@@ -3,11 +3,11 @@ import { Searcher } from './components/searcher';
 import { useCharacterListController } from './hooks/use-characters-list-controller';
 
 const CharactersList = () => {
-  const { data, error, isLoading, searchText, setSearchText } = useCharacterListController();
+  const { data, searchText, setSearchText } = useCharacterListController();
 
   return (
     <section className="c-characters-list">
-      <Searcher onChange={setSearchText} value={searchText} />
+      <Searcher onChange={setSearchText} value={searchText} totalResults={data?.length ?? 0} />
       <div className="c-characters-list__container">
         {data?.map((character) => (
           <div key={character.id}>
